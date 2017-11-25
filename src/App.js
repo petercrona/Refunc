940b213ddeb50122ba999d2f6b891cd41292d924
Peter Crona <petercrona89@gmail.com>
2017-11-25 18:12:23 +0100

    add tic-tac-toe example

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
+import TicTacToe from './TicTacToe';
 
 class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
                 <Counter />
+                <hr />
+                <TicTacToe />
             </div>
         );
     }
